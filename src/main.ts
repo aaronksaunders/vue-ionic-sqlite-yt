9dc9d0acfb5c9012a97d1dab03e22ffb1270413d
Aaron Saunders <aaronksaunders@gmail.com>
2023-06-11 23:34:28 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,94 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue } from "@ionic/vue";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/vue/css/core.css';
+import "@ionic/vue/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/vue/css/normalize.css';
-import '@ionic/vue/css/structure.css';
-import '@ionic/vue/css/typography.css';
+import "@ionic/vue/css/normalize.css";
+import "@ionic/vue/css/structure.css";
+import "@ionic/vue/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/float-elements.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
+import "@ionic/vue/css/padding.css";
+import "@ionic/vue/css/float-elements.css";
+import "@ionic/vue/css/text-alignment.css";
+import "@ionic/vue/css/text-transformation.css";
+import "@ionic/vue/css/flex-utils.css";
+import "@ionic/vue/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
-router.isReady().then(() => {
-  app.mount('#app');
+///
+import { Capacitor } from "@capacitor/core";
+import {
+  CapacitorSQLite,
+  SQLiteConnection,
+  SQLiteDBConnection,
+} from "@capacitor-community/sqlite";
+import { JeepSqlite } from "jeep-sqlite/dist/components/jeep-sqlite";
+
+customElements.define("jeep-sqlite", JeepSqlite);
+console.log(`after customElements.define`);
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const platform = Capacitor.getPlatform();
+    const sqlite = new SQLiteConnection(CapacitorSQLite);
+
+    // WEB SPECIFIC FUNCTIONALITY
+    if (platform === "web") {
+      // Create the 'jeep-sqlite' Stencil component
+      const jeepSqliteEl = document.createElement("jeep-sqlite");
+      document.body.appendChild(jeepSqliteEl);
+      await customElements.whenDefined("jeep-sqlite");
+      console.log(`after customElements.whenDefined`);
+
+      // Initialize the Web store
+      await sqlite.initWebStore();
+      console.log(`after initWebStore`);
+    }
+
+    // example: database creation with standard SQLite statements
+    const ret = await sqlite.checkConnectionsConsistency();
+    const isConn = (await sqlite.isConnection("db_vite", false)).result;
+    let db = null;
+    if (ret.result && isConn) {
+      db = await sqlite.retrieveConnection("db_vite", false);
+    } else {
+      db = await sqlite.createConnection(
+        "db_vite",
+        false,
+        "no-encryption",
+        1,
+        false
+      );
+    }
+
+    // manipulate database
+    await db.open();
+    console.log(`db: db_vite opened`);
+    const queryCreateTable = `
+      CREATE TABLE IF NOT EXISTS test (
+      id INTEGER PRIMARY KEY NOT NULL,
+      name TEXT NOT NULL
+      );
+    `;
+    const respCT = await db.execute(queryCreateTable);
+    console.log(`res: ${JSON.stringify(respCT)}`);
+
+    await sqlite.closeConnection("db_vite", false);
+
+    // ionic app initialization
+    const app = createApp(App).use(IonicVue).use(router);
+    router.isReady().then(() => {
+      app.mount("#app");
+    });
+  } catch (e) {
+    console.log((e as any).message);
+  }
 });
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,33 +2,195 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Vue + SQLITE</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <template v-if="editItem">
+        <ion-item>
+          <ion-input type="text" v-model="inputName"></ion-input>
+          <ion-button @click="() => setEditItem(undefined)">CANCEL</ion-button>
+          <ion-button @click="updateItem">UPDATE</ion-button>
+        </ion-item>
+      </template>
+      <template v-else>
+        <ion-item>
+          <ion-input type="text" v-model="inputName"></ion-input>
+          <ion-button slot="end" @click="addItem">ADD</ion-button>
+        </ion-item>
+      </template>
+      <h3>THE DATA</h3>
+      <ion-item v-for="item in items" :key="item?.id">
+        <ion-label>
+          {{ item.name }}
+        </ion-label>
+        <ion-button @click="() => setEditItem(item)">EDIT</ion-button>
+        <ion-button @click="() => deleteItem(item.id)">DELETE</ion-button>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+type SQLItem = {
+  id: number;
+  name: string;
+}
+import { ref } from "vue";
+import {
+  IonContent,
+  onIonViewDidEnter,
+  onIonViewWillLeave,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonInput,
+} from "@ionic/vue";
+import {
+  CapacitorSQLite,
+  SQLiteConnection,
+  SQLiteDBConnection,
+} from "@capacitor-community/sqlite";
+const items = ref<SQLItem[] | undefined>();
+const db = ref<SQLiteDBConnection>();
+const sqlite = ref<SQLiteConnection>();
+const inputName = ref<string>("");
+
+const editItem = ref<SQLItem | undefined>(undefined);
+
+onIonViewDidEnter(async () => {
+  sqlite.value = new SQLiteConnection(CapacitorSQLite);
+  const ret = await sqlite.value.checkConnectionsConsistency();
+  const isConn = (await sqlite.value.isConnection("db_vite", false)).result;
+
+  if (ret.result && isConn) {
+    db.value = await sqlite.value.retrieveConnection("db_vite", false);
+  } else {
+    db.value = await sqlite.value.createConnection(
+      "db_vite",
+      false,
+      "no-encryption",
+      1,
+      false
+    );
+  }
+
+  loadData();
+});
+
+onIonViewWillLeave(async () => {
+  await sqlite.value?.closeConnection("db_vite", false);
+});
+
+/**
+ * do an insert to database
+ */
+const deleteItem = async (id: number) => {
+  try {
+    await db.value?.open();
+    // add test record to db
+    await db.value?.query(`DELETE FROM test WHERE id=?;`, [id]);
+
+    // update ui
+    const respSelect = await db.value?.query(`SELECT * FROM test`);
+    items.value = respSelect?.values;
+  } catch (error) {
+    alert((error as Error).message);
+  } finally {
+    inputName.value = "";
+    await db.value?.close();
+  }
+};
+
+/**
+ * do an insert to database
+ */
+const addItem = async () => {
+  try {
+    await db.value?.open();
+    // add test record to db
+    await db.value?.query(`INSERT INTO test (id,name) values (?,?);`, [
+      Date.now(),
+      inputName.value,
+    ]);
+
+    // update ui
+    const respSelect = await db.value?.query(`SELECT * FROM test;`);
+    items.value = respSelect?.values;
+  } catch (error) {
+    alert((error as Error).message);
+  } finally {
+    inputName.value = "";
+    await db.value?.close();
+  }
+};
+
+/**
+ *
+ * @param item
+ */
+const setEditItem = (item: SQLItem | undefined) => {
+  if (item) {
+    editItem.value = item;
+    inputName.value = item.name;
+  } else {
+    editItem.value = undefined;
+    inputName.value = "";
+  }
+};
+
+/**
+ * update entry in database
+ */
+const updateItem = async () => {
+  try {
+    debugger;
+    await db.value?.open();
+    // update test record to db
+    await db.value?.query(`UPDATE test SET name=? WHERE id=?`, [
+      inputName.value,
+      editItem.value?.id,
+    ]);
+
+    setEditItem(undefined);
+
+    // update ui
+    const respSelect = await db.value?.query(`SELECT * FROM test;`);
+    items.value = respSelect?.values;
+  } catch (error) {
+    alert((error as Error).message);
+  } finally {
+    inputName.value = "";
+    await db.value?.close();
+  }
+};
+
+/**
+ * do a select of the database
+ */
+const loadData = async () => {
+  try {
+    // query db
+    await db.value?.open();
+    const respSelect = await db.value?.query(`SELECT * FROM test`);
+    items.value = respSelect?.values;
+  } catch (error) {
+    alert((error as Error).message);
+  } finally {
+    await db.value?.close();
+  }
+};
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +206,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
